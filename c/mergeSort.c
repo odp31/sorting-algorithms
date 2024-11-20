50dc212e0a09db54cf787a3cf912c8ea92b6d0f7
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-20 12:51:54 -0500

    Update mergeSort.c

--- c/mergeSort.c
+++ c/mergeSort.c
@@ -1,2 +1,59 @@
 // time complexity; O(n log n)
 // space complexity; O(n) 
+
+#include <stdio.h>
+
+void merge(int arr[], int l, int m, int r)
+{
+  int i, j, k;
+  int n1 = m - l + 1;
+  int n2 = r - m;
+  // create temp arrays
+  int L[n1], R[n2];
+
+  // copy data to temp arrays 
+  for (i = 0; i < n1; i++)
+    L[i] = arr[l+i];
+  for(j = 0; j < n2; j++)
+    R[j] = arr[m+1+j];
+
+  i = 0;
+  j = 0;
+  k = l;
+  while (i < n1 && j < n2){
+    if (L[i] <= R[j]){
+      arr[k] = L[i];
+      i++;
+    }
+    else {
+      arr[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+while (i < n1)
+{
+  arr[k] = L[i];
+  i ++;
+  k++;
+}
+while (j < n2){
+arr[k] = R[j];
+j++; 
+k++;
+}
+while(j < n2){
+arr[k] = R[j];
+j++;
+k++;
+}
+}
+void mergeSort(int arr[], int l, int r)
+{
+  if(l < r) {
+    int m = l + (r - l) / 2;
+    mergeSort(arr, l, m);        // sort first and second halves
+    mergeSort(arr, m+1, r);
+    merge(arr, l, m, r);        // merge sorted halves 
+  }
+}
