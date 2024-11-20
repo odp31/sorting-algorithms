13623d3f8567dba5092568d8ba69a229a9ca0e40
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-20 12:39:51 -0500

    Create bubbleSort.c

---
+++ c/bubbleSort.c
@@ -0,0 +1,15 @@
+// time complexity; O(n^2)
+// space: o(1)
+
+#include <stdio.h>
+void bubbleSort(int arr[], int n)
+{
+  int i, j;
+  for(i = 0; i < n - 1; i++)
+    for(j = 0; j < n - i - 1; j++)
+      if (arr[j] > arr[j + 1]){
+        int temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+}
