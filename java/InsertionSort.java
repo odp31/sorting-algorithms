3b8bb08bba5afab916af08f18262526aaab05416
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-20 11:57:10 -0500

    Create InsertionSort.java

---
+++ java/InsertionSort.java
@@ -0,0 +1,22 @@
+// time complexity; O(n^2)
+// space complexity; O(1)
+
+public class InsertionSort
+  {
+    public static void insertionSort(int[] arr)
+    {
+      int n = arr.length;
+      for (int i = 1; i < n; ++i)
+        {
+          int key = arr[i];
+          int j = i - 1; 
+
+          while (j >= 0 && arr[j] > key)
+            {
+              arr[j+1] = arr[j];
+              j = j - 1;
+            }
+          arr[j+1] = key;
+        }
+    }
+  }
