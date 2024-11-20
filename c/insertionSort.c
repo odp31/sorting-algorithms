9b3ba2e0ec44d2466330997e0be716cb8727e695
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-20 12:41:30 -0500

    Create insertionSort.c

---
+++ c/insertionSort.c
@@ -0,0 +1,18 @@
+// time complexity; o(n^2)
+// space complexity; O(1)
+
+#include <stdio.h>
+
+void insertionSort(int arr[], int n)
+{
+  int i, key, j;
+  for (i = 1; i < n; i++)
+    key = arr[i];
+    j = i - 1;
+
+    while (j >= 0 && arr[j] > key){
+      arr[j+1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+}
