c1d26ca8a2a6c238fae029320b56ad9099d3b034
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-21 12:46:12 -0500

    Create insertionSort.js

---
+++ javascript/insertionSort.js
@@ -0,0 +1,19 @@
+// time complexity; O(n^2)
+// space complexity; O(1)
+
+function insertionSort(arr)
+  {
+  const n = arr.length;
+  for(let i = 1; i < n; i++)
+    {
+      let key = arr[i];
+      let j = i - 1;
+      while (j >= 0 && arr[j] > key)
+        {
+          arr[j+1] = arr[j];
+          j--;
+        }
+      arr[j+1] = key;
+    }
+  return arr;
+}
