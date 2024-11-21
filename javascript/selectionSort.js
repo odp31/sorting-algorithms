90ba7f9b44e7d1310afd914a5522293feacdf1e7
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-21 12:44:11 -0500

    Create selectionSort.js

---
+++ javascript/selectionSort.js
@@ -0,0 +1,20 @@
+// time complexity; o(n^2)
+// space complexity; O(1)
+
+function selectionSort(arr)
+{
+  const n = arr.length;
+  for(let i = 0; i < n - 1; i++)
+    {
+      let minIndex = i;
+      for(let j = i + 1; j < n; j++)
+        {
+          if(arr[j] < arr[minIndex])
+          {
+            minIndex = j;
+          }
+        }
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  return arr;
+}
